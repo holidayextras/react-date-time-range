664e2bd34721cc4de677b8faf24413f6073c193f
Ollie Buck <git@olliebuck.com>
2015-10-15 13:43:08 +0100

    get example to a working state

--- doc/example.js
+++ doc/example.js
@@ -5,6 +5,43 @@ var DateTimeRange = require('../src/DateTimeRange.jsx');
 var DateTimeGroup = require('react-date-time-group');
 
 var Example = React.createClass({
+  getInitialState: function() {
+    return {
+      firstRange: {
+        // With no end, the end will be defaulted x days in the future.
+        // If the user picks an end date, the end will only be moved
+        // if it's no longer valid given the start... ??? ..?
+        // And we can push the start back up into shared state at the engine level
+        // if needed.
+        // Remember to delete all these comments talking about usage specifics
+        start: new Date(),
+        duration: 8
+      },
+      secondRange: {
+        start: new Date(2015, 8, 9, 14, 30),
+        end: new Date(2015, 8, 20, 11, 15)
+      }
+    };
+  },
+
+  changeFirstExample: function(start, end) {
+    this.setState({
+      firstRange: {
+        start: start,
+        end: end
+      }
+    });
+  },
+
+  changeSecondExample: function(start, end) {
+    this.setState({
+      secondRange: {
+        start: start,
+        end: end
+      }
+    });
+  },
+
   render: function() {
     return (
       <div className="container">
@@ -13,10 +50,34 @@ var Example = React.createClass({
         </div>
 
         <h2>Basic example</h2>
-        <DateTimeRange>
+        <DateTimeRange start={this.state.firstRange.start} end={this.state.firstRange.end} duration={6} onChange={this.changeFirstExample}>
           <DateTimeGroup />
           <DateTimeGroup />
         </DateTimeRange>
+
+        <h2>Overriding settings</h2>
+        <DateTimeRange start={this.state.secondRange.start} end={this.state.secondRange.end} onChange={this.changeSecondExample}>
+          <DateTimeGroup
+            dateLabel="Start Date"
+            timeLabel="Start Time"
+            dateStart={new Date(2015, 8, 1)}
+            dateEnd={new Date(2015, 8, 14)}
+            dateExclusions={[new Date(2015, 8, 5), new Date(2015, 8, 6), new Date(2015, 8, 12), new Date(2015, 8, 13)]}
+            timeName="StartTime" />
+          <DateTimeGroup
+            dateLabel="End Date"
+            timeLabel="End Time"
+            dateStart={new Date(2015, 8, 19)}
+            dateEnd={new Date(2015, 8, 26)}
+            timeStart={900}
+            timeEnd={1800}
+            timeStep={15}
+            timeClassName="foo"
+            timeName="EndTime" />
+        </DateTimeRange>
+        <ul>
+          <li>an "end" prop is passed in, so the end date will not automatically fast-forward if the start date changes</li>
+        </ul>
       </div>
     );
   }
--- src/DateTimeRange.jsx
+++ src/DateTimeRange.jsx
@@ -1,8 +1,92 @@
+'use strict';
+
 var React = require('react');
+var DateTimeGroup = require('react-date-time-group');
 
 var DateTimeRange = React.createClass({
+  propTypes: {
+    start: React.PropTypes.instanceOf(Date),
+    end: React.PropTypes.instanceOf(Date),
+    duration: React.PropTypes.number,
+    onChange: React.PropTypes.func,
+    children: React.PropTypes.array
+  },
+
+  getDefaultProps: function() {
+    return {
+      duration: 10
+    };
+  },
+
+  identifyStartAndEndDateChildComponents: function() {
+    var rangeStartComponent, rangeEndComponent;
+
+    React.Children.forEach(this.props.children, function(child) {
+      if (child.type === DateTimeGroup) {
+        if (!rangeStartComponent) {
+          rangeStartComponent = child;
+          return;
+        }
+
+        if (!rangeEndComponent) {
+          rangeEndComponent = child;
+          return;
+        }
+
+        throw new Error('Expected exactly two DateTimeGroup child components of DateTimeRange, but got more');
+      }
+    });
+
+    if (!rangeStartComponent || !rangeEndComponent) {
+      throw new Error('Expected exactly two DateTimeGroup child components of DateTimeRange, but got less');
+    }
+
+    return {
+      start: rangeStartComponent,
+      end: rangeEndComponent
+    };
+  },
+
+  assumedEndDate: function() {
+    var endDate = new Date(this.props.start);
+    endDate.setDate(endDate.getDate() + this.props.duration);
+    return endDate;
+  },
+
+  earliestDate: function(dateOne, dateTwo) {
+    return dateOne > dateTwo ? dateOne : dateTwo;
+  },
+
+  childrenWithAttachedBehaviour: function() {
+    var startAndEnd = this.identifyStartAndEndDateChildComponents();
+    var self = this;
+
+    return React.Children.map(this.props.children, function(child) {
+      if (child === startAndEnd.start) {
+        return React.cloneElement(child, {
+          onChange: function(newDate) {
+            self.props.onChange(newDate, self.props.end);
+          },
+          value: self.props.start
+        });
+      }
+
+      if (child === startAndEnd.end) {
+        return React.cloneElement(child, {
+          onChange: function(newDate) {
+            self.props.onChange(self.props.start, newDate);
+          },
+          value: self.props.end || self.assumedEndDate(),
+          dateStart: self.earliestDate(self.props.start, child.props.dateStart)
+        });
+      }
+
+      return child;
+    });
+  },
+
   render: function() {
-    return (<div>{this.props.children}</div>);
+    return (<div>{this.childrenWithAttachedBehaviour()}</div>);
   }
 });
 
